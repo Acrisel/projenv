f6d3a8af8e08c3c6f4b049d66b0bd851be022e41
Uri <uri@acrisel.com>
2015-05-29 18:54:42 -0500

    clean

--- __init__.py
+++
@@ -1,6 +0,0 @@
-'''
-Created on Apr 26, 2015
-
-@author: uri
-'''
-import environ
--- test/unittest1.py
+++
@@ -1,8 +0,0 @@
-'''
-Created on Apr 26, 2015
-
-@author: uri
-'''
-from environ import Environ
-env=Environ()
-print (env)
