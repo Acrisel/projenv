bfb3009406ab301d9a42d0ce798779b194bfc28b
Arnon Sela <arnon@acrisel.com>
2015-09-06 13:18:24 -0500

    input bug fix

--- environ/example/example.py
+++ environ/example/example.py
@@ -4,7 +4,7 @@ Created on Jun 15, 2015
 @author: arnon
 '''
 
-from projenv import Environ
+from projenv import Environ, EnvVar
 import os.path
 import logging
 
@@ -26,11 +26,13 @@ handler.setFormatter(formatter)
 logger.addHandler(handler)
 logger.setLevel(logging.DEBUG)
 
-    
+input_environ=[EnvVar(name='A1',value='133', input=True),]
+
 root_loc=os.path.dirname(__file__)
 module_loc=os.path.join(root_loc, 'envexample', 'lvl1', 'lvl2', 'lvl3')
-module_env=Environ(osenv=True, trace_env=['A1', 'B1', 'C1'], logclass='Example') #, ulogger=logger)
+module_env=Environ(osenv=True, trace_env=['A1', ], logclass='Example') #, ulogger=logger)
 module_env.loads(path=module_loc)
+module_env.update_env(input_environ=input_environ)
 
 print('POST ENVIRON:', repr(module_env))
 
